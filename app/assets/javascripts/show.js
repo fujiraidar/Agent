2a748e593fd8d915f60476fcf8f70da3e14d7ca0
fujiraidar <fujirider@icloud.com>
2020-02-12 22:09:26 +0000

    markdownの完成、目次完成

--- app/assets/javascripts/show.js
+++ app/assets/javascripts/show.js
@@ -1,18 +1,6 @@
 
 
 
-$(document).on("turbolinks:load",function(){
-	var md = sanitize($('<%= @info.body %>').val());
-    var html = marked(md)
-    $('#show_marked').html(html);
-    $('#show_marked pre code').each(function(i, e) {
-        $(e).text(unsanitize($(e).text()));
-        hljs.highlightBlock(e, e.className);
-    });
-});
-
-
-
 
 
 
